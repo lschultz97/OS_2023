10e198dcddf28256ee72f15e28cef8c5a82f7c17
Nadi25 <nadine-arzt@gmx.de>
2023-11-17 10:54:41 +0100

    answered

--- test_file.R
+++ test_file.R
@@ -8,3 +8,8 @@
 # und dir ?
 
 # created a new branch
+
+# Mir geht es auch gut :)
+# Die Kombination aus R und GitHub wäre eigentlich eine coole
+# nerdy Version, um zu kommunizieren
+# WhatsApp ist bestimmt bald out
